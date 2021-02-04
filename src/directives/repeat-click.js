47e429bbcc1ab8b8f33be448c0110ff042575fc1
qiufeihong <36500514+qiufeihong2018@users.noreply.github.com>
2021-02-04 19:11:00 +0800

    Update repeat-click.js

--- src/directives/repeat-click.js
+++ src/directives/repeat-click.js
@@ -1,5 +1,7 @@
 import { once, on } from 'element-ui/src/utils/dom';
+// 中文名叫重复点击，为了避免短时间内重复点击，在每个按钮上绑定清除点击事件，当两次点击小于100ms时，清除点击。类似于防抖
 
+// 这里有一篇解析这个自定义工具的博文：https://blog.csdn.net/weixin_34220623/article/details/87945747
 export default {
   bind(el, binding, vnode) {
     let interval = null;
