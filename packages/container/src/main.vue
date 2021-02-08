5259812c1c26817b44605416160767fdc39244bc
qiufeihong <36500514+qiufeihong2018@users.noreply.github.com>
2021-02-08 13:44:47 +0800

    Update main.vue

--- packages/container/src/main.vue
+++ packages/container/src/main.vue
@@ -9,18 +9,20 @@
     name: 'ElContainer',
 
     componentName: 'ElContainer',
-
+// 只提供一个参数方向
     props: {
       direction: String
     },
 
     computed: {
       isVertical() {
+   //   判断是横向还是竖向
         if (this.direction === 'vertical') {
           return true;
         } else if (this.direction === 'horizontal') {
           return false;
         }
+        //header和footer是横向的，其他都是竖向
         return this.$slots && this.$slots.default
           ? this.$slots.default.some(vnode => {
             const tag = vnode.componentOptions && vnode.componentOptions.tag;
