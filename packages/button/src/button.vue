b0c232ba5a27af1ef699c3da7691d762bb02e067
qiufeihong <36500514+qiufeihong2018@users.noreply.github.com>
2021-02-08 15:39:57 +0800

    Update button.vue

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -36,40 +36,54 @@
     },
 
     props: {
+    //类型:primary/success/warning/danger/info/text
       type: {
         type: String,
         default: 'default'
       },
+     // 尺寸:mini/small/medium
       size: String,
-      icon: {
+     // 图标类型
+     icon: {
         type: String,
         default: ''
       },
+//      原生button上的属性：button/submit/reset
       nativeType: {
         type: String,
         default: 'button'
       },
+      //是否加载
       loading: Boolean,
+      //是否禁用
       disabled: Boolean,
+      //是否朴素
       plain: Boolean,
+      //是否默认聚焦
       autofocus: Boolean,
+      //是否圆角
       round: Boolean,
+      //是否圆形
       circle: Boolean
     },
 
     computed: {
       _elFormItemSize() {
+      //父表单项的尺寸
         return (this.elFormItem || {}).elFormItemSize;
       },
       buttonSize() {
+      //本身的尺寸、父表单项的尺寸和全局element的尺寸
         return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
       },
       buttonDisabled() {
+      //本身是否禁用和父表单是否禁用
         return this.disabled || (this.elForm || {}).disabled;
       }
     },
 
     methods: {
+    //点击事件
       handleClick(evt) {
         this.$emit('click', evt);
       }
