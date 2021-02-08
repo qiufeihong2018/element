51522477460e13ee1cfa9ac6cc222902b7876e80
qiufeihong <36500514+qiufeihong2018@users.noreply.github.com>
2021-02-08 17:13:53 +0800

    Update radio.vue

--- packages/radio/src/radio.vue
+++ packages/radio/src/radio.vue
@@ -20,6 +20,7 @@
         'is-checked': model === label
       }"
     >
+// aria-hidden的属性参见    https://developer.mozilla.org/zh-CN/docs/Web/Accessibility/ARIA/ARIA_Techniques/%E4%BD%BF%E7%94%A8aria-hidden%E5%B1%9E%E6%80%A7
       <span class="el-radio__inner"></span>
       <input
         ref="radio"
@@ -63,11 +64,17 @@
     componentName: 'ElRadio',
 
     props: {
+ //   单选框绑定的值 v-model
       value: {},
+      //每个标签值
       label: {},
+      //是否禁用
       disabled: Boolean,
+      // name属性
       name: String,
+      //是否有边框
       border: Boolean,
+      //尺寸：mini/small/medium
       size: String
     },
 
@@ -77,6 +84,7 @@
       };
     },
     computed: {
+    //是否是组
       isGroup() {
         let parent = this.$parent;
         while (parent) {
@@ -94,37 +102,45 @@
           return this.isGroup ? this._radioGroup.value : this.value;
         },
         set(val) {
+//        单选框组dispatch发送input事件
           if (this.isGroup) {
             this.dispatch('ElRadioGroup', 'input', [val]);
           } else {
+//          单选框用$emit发送input事件
             this.$emit('input', val);
           }
           this.$refs.radio && (this.$refs.radio.checked = this.model === this.label);
         }
       },
+//      表单项尺寸
       _elFormItemSize() {
         return (this.elFormItem || {}).elFormItemSize;
       },
+//      单选按钮的尺寸
       radioSize() {
         const temRadioSize = this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
         return this.isGroup
           ? this._radioGroup.radioGroupSize || temRadioSize
           : temRadioSize;
       },
+      // 单选按钮组、单选按钮、表单来看是否禁用
       isDisabled() {
         return this.isGroup
           ? this._radioGroup.disabled || this.disabled || (this.elForm || {}).disabled
           : this.disabled || (this.elForm || {}).disabled;
       },
       tabIndex() {
+//      不禁用，是按钮组，是选中的，值为0，其余为1
         return (this.isDisabled || (this.isGroup && this.model !== this.label)) ? -1 : 0;
       }
     },
 
     methods: {
+    // 选择后，触发事件
       handleChange() {
         this.$nextTick(() => {
           this.$emit('change', this.model);
+//          如果是按钮组，触发父级的change事件
           this.isGroup && this.dispatch('ElRadioGroup', 'handleChange', this.model);
         });
       }
